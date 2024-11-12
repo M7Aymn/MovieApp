b2a2ab25c6385fc33b9537b9f28df5f9f687e6a7
M7Aymn <modyhemead@gmail.com>
2024-11-12 10:02:43 +0200

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Mohamed Ayman on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
